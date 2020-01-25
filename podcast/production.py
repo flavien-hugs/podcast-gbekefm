2db99036bc925e5846678505661b7dbb9f4650e1
flavien-hugs <geekactux@gmail.com>
2020-01-25 16:19:19 +0000

    add folder collectstatic

--- podcast/production.py
+++ podcast/production.py
@@ -15,7 +15,7 @@ DATABASES['default'] = dj_database_url.config()
 INSTALLED_APPS += ['whitenoise.runserver_nostatic']
 
 # 'django.middleware.security.SecurityMiddleware',
-MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+MIDDLEWARE_CLASSES = ('whitenoise.middleware.WhiteNoiseMiddleware',)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
