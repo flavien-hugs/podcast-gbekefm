91a78e26dc3b8de47345fd4485534994665bb2cf
flavien-hugs <geekactux@gmail.com>
2020-01-25 16:33:05 +0000

    add folder collectstatic

--- podcast/settings.py
+++ podcast/settings.py
@@ -132,10 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'static',
