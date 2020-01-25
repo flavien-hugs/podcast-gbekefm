76068d1eba53eb2f52c56a5511407b280ba6c644
flavien-hugs <geekactux@gmail.com>
2020-01-25 15:44:22 +0000

    delete folder collectstatic

--- podcast/settings.py
+++ podcast/settings.py
@@ -139,3 +139,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'static',
 ]
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
